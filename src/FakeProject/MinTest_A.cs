91b19944361634ca0700373351881f356cb7e0c0
Kyle Wesley <kyle.wesley1@gmail.com>
2025-05-08 14:09:38 -0600

    Add minification test files

---
+++ src/FakeProject/MinTest_A.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FakeProject
+{
+    //Single line comments get included too
+    public class MinTest_A : IMinTestA
+    {
+        readonly MinTest_B _B = new();
+        
+        /// <summary>
+        /// Do some important work
+        /// </summary>
+        public void DoSomething()
+        {
+            _B.DoThingB();
+        }
+    }
+}
